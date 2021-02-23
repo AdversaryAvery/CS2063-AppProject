05953b4913357893d828d261c06eb24a23969ad8
Raven-Lee Mills <rayvenmills.1998@gmail.com>
2021-02-22 22:04:10 -0400

    Added logging to welcome activity

--- app/src/main/java/com/Team13/myapplication/WelcomeActivity.java
+++ app/src/main/java/com/Team13/myapplication/WelcomeActivity.java
@@ -5,15 +5,18 @@ import androidx.constraintlayout.solver.widgets.Guideline;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
+    private static final String TAG = "WELCOME_ACTIVITY";
 
     private Button playGameButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "onCreate called");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
         playGameButton = findViewById(R.id.btnLaunchGame);
@@ -21,6 +24,7 @@ public class WelcomeActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(WelcomeActivity.this, GameActivity.class);
+                Log.i(TAG, "starting Game Activity");
                 startActivity(intent);
             }
         });
